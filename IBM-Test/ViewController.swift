4eac21e02922d4435201cfc6c121c744e14d1716
Gustavo Ludtke <gustavoludtke.dev@gmail.com>
2020-08-04 23:41:38 -0300

    Base app structure added

---
+++ IBM-Test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IBM-Test
+//
+//  Created by Gustavo Ludtke on 04/08/20.
+//  Copyright © 2020 IBM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
